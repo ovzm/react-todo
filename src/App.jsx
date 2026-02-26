17973bec40dbb3607a4a3c43df119e1d23843b7e
Victor Zawacki Monteiro <victorzawacki9@gmail.com>
2026-02-26 16:56:24 -0300

    app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs'
+
+const API = "http://localhost:5000"
 
 function App() {
 
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
   return (
-    <>
-    </>
+    <div className='App'>
+      <div className="todo-header">
+        <h1>React ToDo</h1>
+      </div>
+      <div className="form-todo">
+        <p>Formulário</p>
+      </div>
+      <div className="list-todo">
+        <p>Lista</p>
+      </div>
+    </div>
   )
 }
 
