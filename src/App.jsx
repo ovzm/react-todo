8d50aa5b694198734349bb9e354a1aae5e52e29f
Victor Zawacki Monteiro <victorzawacki9@gmail.com>
2026-03-11 15:37:24 -0300

    Actions

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,30 @@ function App() {
     setTime("");
   }
 
+  const handleDelete = async (id) => {
+    
+    await fetch(API + "/todos" + id, {
+      method: "DELETE",
+    })
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id))
+  }
+
+  const handleEdit = async (todo) => {
+
+    todo.done = !todo.done
+    
+    const data = await fetch(API + "/todos" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    setTodos((prevState) => prevState.map((t) => (t.id === data.id ? (t = data) : t )))
+  }
+
   if(loading) {
     return <p>Carregando...</p>
   }
@@ -87,10 +111,10 @@ function App() {
             <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
             <p>Duração: {todo.time}</p>
             <div className="actions">
-              <span>
+              <span onClick={() => handleEdit(todo)}>
                 {!todo.done ? <BsBookmarkCheck/> : <BsBookmarkCheckFill/>}
               </span>
-              <BsTrash/>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
             </div>
           </div>
         ))}
